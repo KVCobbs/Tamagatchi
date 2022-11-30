a8594441a34ee84b074001bcf2ba1cb1b1e17dd4
KVCobbs <kenyacobbs9001@gmail.com>
2022-11-30 14:45:45 -0500

    This are actually updating

--- script.js
+++ script.js
@@ -2,40 +2,52 @@
 let avacado = 2;
 let artichoke = 5;
 let bagel = 8; 
-let beer = -2
+let beer = -6;
 let foods = [avacado, artichoke, bagel, beer,];
+
+//Time to create poop 
+let poop = -10;
+
+//create a timer for poop to appear 
 
 
 //just trying to write an object out with this one 
 let tamagatchi = {
     // Need to ask the boss about this 
     name: "Tama-chan",
-    age: 10, 
+    age: 5, 
     hunger: 50,
-    health: 100,
+    health: 40,
     feeding: function() {
         for (let i = 0; i < foods.length; i++) {
             //Thanks to weily I converted the foods elements into numbers using parseInt and then added 
             //resulting in the different values of fed according to the food and the hunger level 
             let gobble = parseInt(foods[i])
-            let fed = this.hunger + gobble
-            console.log(fed)
+            this.hunger += gobble
+            console.log(this.hunger)
             }
-        }
+        },
+        cleaning: function(){
+          // if (this.health < 50) {
+          //   console.log("It stinks in here")
+          // }
+          this.health -= 10 
+          console.log(this.health)
+        },
 };
 
-
-//ok so to access a method you need the objectName.method
-
-
 //I need to make the buttons 
-
+//feeding
  const feedButton = document.getElementById("feed");
-console.log(feedButton)
-
 feedButton.addEventListener("click", function() {
   //ok so to access a method you need the objectName.method
-  tamagatchi.feeding()
+  tamagatchi.feeding();
+});
+
+//cleaning 
+const cleanButton = document.getElementById("clean");
+cleanButton.addEventListener("click", function(){
+  tamagatchi.cleaning();
 });
 
 
