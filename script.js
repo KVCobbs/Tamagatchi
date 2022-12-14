f11e1cc2cd97845054774e48e73d1a45f6d932b1
KVCobbs <kenyacobbs9001@gmail.com>
2022-12-14 11:03:26 -0500

    Worked on features more and buttons work

--- script.js
+++ script.js
@@ -8,8 +8,26 @@ let foods = [avacado, artichoke, bagel, beer,];
 //Time to create poop 
 let poop = -10;
 
+//look into webstorage as a way to mimic the storage 
 //create a timer for poop to appear 
+// const startTime = 5; //start time = 5minutes 
+// let currentTime = startTime * 1000; //start time  * 1000 to represent seconds 
 
+// setInterval(updateTimer, 1000)
+
+// function updateTimer(){
+//   const minutes = Math.floor(currentTime / 1000 );
+//   let seconds = currentTime % 60; 
+//   currentTime--;
+// }
+
+// updateTimer()
+
+// function poop () {
+//           let pooped = -10
+//           this.health += pooped
+//           console.log(this.health)
+// }
 
 //just trying to write an object out with this one 
 let tamagatchi = {
@@ -17,7 +35,10 @@ let tamagatchi = {
     name: "Tama-chan",
     age: 5, 
     hunger: 50,
-    health: 40,
+    health: 100,
+        checkStats: function(){
+          console.log(`${tamagatchi.name} is ${tamagatchi.age} years old, has ${tamagatchi.hunger} hunger and ${tamagatchi.health} health`)
+          },
     feeding: function() {
         for (let i = 0; i < foods.length; i++) {
             //Thanks to weily I converted the foods elements into numbers using parseInt and then added 
@@ -27,30 +48,44 @@ let tamagatchi = {
             console.log(this.hunger)
             }
         },
-        cleaning: function(){
-          // if (this.health < 50) {
-          //   console.log("It stinks in here")
-          // }
-          this.health -= 10 
+        poop: function() {
+          let pooped = -10
+          this.health += pooped
           console.log(this.health)
-        },
-};
+        }
+}
 
 //I need to make the buttons 
 //feeding
- const feedButton = document.getElementById("feed");
+const feedButton = document.getElementById("feed");
 feedButton.addEventListener("click", function() {
   //ok so to access a method you need the objectName.method
   tamagatchi.feeding();
 });
 
-//cleaning 
-const cleanButton = document.getElementById("clean");
-cleanButton.addEventListener("click", function(){
-  tamagatchi.cleaning();
+const checkButton = document.getElementById("check");
+checkButton.addEventListener("click", function() {
+  tamagatchi.checkStats()
 });
 
+const poopButton = document.getElementById("clean");
+poopButton.addEventListener("click", function() {
+  tamagatchi.poop()
+});
+
+// //cleaning 
+// const cleanButton = document.getElementById("clean");
+// cleanButton.addEventListener("click", function(){
+//   tamagatchi.cleaning();
+// });
 
 
-
-
+  // if (this.health <= 50 ) {
+  //           console.log("It stinks in here")
+  //         } else if (this.health < 20) {
+  //           console.log("Do you plan to clean up my poop?")
+  //         }
+  //         //So originally I was just restating an piece of code. With this i am updating a my code 
+  //         this.health -= 10 
+  //         console.log(this.health)
+  //       },
